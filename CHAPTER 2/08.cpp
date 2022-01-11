45b8bd9d3f9c3f061431129df11149b0103b9ec2
yulimmm <89307538+yulimmm@users.noreply.github.com>
2022-01-11 17:53:55 +0900

    Update 08.cpp

--- CHAPTER 2/08.cpp
+++ CHAPTER 2/08.cpp
@@ -7,21 +7,19 @@ int main() {
 	char name[50];
 	char longestName[50];
 
-	cout << "5명의 이름을 ';'으로 구분하여 입력하세요" << endl;
-	cout << ">>";
+	cout << "5 명의 이름을 ';'으로 구분하여 입력하세요"<<endl<<">>";
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 1; i <= 5; i++) {
 		cin.getline(name, 50, ';');
-		cout << i + 1 << " : " << name << endl;
-		if (i == 0) {
-			strcpy_s(longestName, name);
+
+		if (i==1) {
+			strcpy_s(longestName,name);
 		}
-		else if (strlen(longestName) < strlen(name)) {
+		else if(strlen(name)>strlen(longestName)){
 			strcpy_s(longestName, name);
 		}
 	}
-
-	cout << "가장 긴 이름은 " << longestName << endl;
+	cout << "가장 긴 이름은 " << longestName;
 
 	return 0;
 }
