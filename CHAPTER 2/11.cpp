cb0a2748ad6f1cebb479bc3f7f8bdc2215539f9c
yulimmm <89307538+yulimmm@users.noreply.github.com>
2022-01-11 18:21:42 +0900

    Create 11.cpp

---
+++ CHAPTER 2/11.cpp
@@ -0,0 +1,18 @@
+//11번
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int k, n = 0;
+	int sum = 0;
+	
+	cout << "끝 수를 입력하세요>>";
+	cin >> n;
+	for (k = 1; k <= n; k++) {
+		sum += k;
+	}
+
+	cout << "1에서 " << n << "까지의 합은 " << sum << "입니다";
+	return 0;
+}
