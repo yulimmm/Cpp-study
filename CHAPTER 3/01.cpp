aa6d0ac3216493ebadd90692f57202b26b0c359d
yulimmm <89307538+yulimmm@users.noreply.github.com>
2022-01-17 15:05:46 +0900

    Update 01.cpp

--- CHAPTER 3/01.cpp
+++ CHAPTER 3/01.cpp
@@ -1,1 +1,30 @@
-z
+#include <iostream>
+using namespace std;
+
+class Tower {
+public:
+	int height;
+	int getHeight();
+	int getHeight(int h);
+	int seoulTower(int h);
+};
+
+int Tower::getHeight(int h) {
+	return h;
+}
+
+int Tower::getHeight() {
+	return 1;
+}
+
+int Tower::seoulTower(int h) {
+	height = h;
+	return h;
+}
+
+int main() {
+	Tower myTower;
+	Tower seoulTower(100);
+	cout << "높이는 " << myTower.getHeight() << "미터" << endl;
+	cout << "높이는" << seoulTower.getHeight() << "미터" << endl;
+}
