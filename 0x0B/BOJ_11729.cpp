8100066882dfca5bd69a33442d2e6e55a82cef1a
yulimmm <dbfla2ee@gmail.com>
2024-07-11 21:25:49 +0900

    edit

--- 0x0B/BOJ_11729.cpp
+++ 0x0B/BOJ_11729.cpp
@@ -1,24 +1,26 @@
-// Authored by : BaaaaaaaaaaarkingDog
-// Co-authored by : -
-// http://boj.kr/f2440915dca04aaa9aec759080516973
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-void func(int a, int b, int n){
-  if(n == 1){
-    cout << a << ' ' << b << '\n';
+//원판 n개를 a번 기둥에서 b번 기둥으로 옮기는 방법을 출력하는 함수 
+void func(int a, int b, int n)
+{
+  if(n == 1) {
+    cout<<a<<' '<<b<<'\n'; 
     return;
   }
   func(a, 6-a-b, n-1);
-  cout << a << ' ' << b << '\n';
+  cout<<a<<' '<<b<<'\n';
   func(6-a-b, b, n-1);
 }
 
-int main(void){
+int main()
+{
   ios::sync_with_stdio(0);
   cin.tie(0);
   int k;
-  cin >> k;
-  cout << (1<<k) - 1 << '\n';
-  func(1, 3, k);
+  cin>>k;
+  cout<<(1<<k)-1<<'\n';
+  func(1,3,k);
+
+  return 0;
 }
