ba702d2ca83d3eafa4faff7231406ab215a00036
yulimmm <89307538+yulimmm@users.noreply.github.com>
2024-08-11 18:26:07 +0900

    Update BOJ_1780.cpp

--- 0x0B/BOJ_1780.cpp
+++ 0x0B/BOJ_1780.cpp
@@ -1,40 +1,53 @@
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
 
-int N;
-int paper[2200][2200];
-int cnt[3]; //-1, 0, 1로 채워진 종이 갯수
+int v[2187][2187];
+int cnt[3] = {0}; // -1. 0. 1
 
-//해당 종이 내부에 같은 숫자로만 채워졌는지 확인하는 함수
-bool check(int x, int y, int n) {
-    for (int i = x; i < x + n; i++)
-        for (int j = y; j < y + n; j++)
-            if (paper[x][y] != paper[i][j])
+bool check(int x, int y, int n)
+{
+    for(int i = x; i < x+n; i++){
+        for(int j = y; j < y+n; j++){
+            if(v[x][y] != v[i][j]){
                 return false;
+            }
+        }
+    }
     return true;
 }
 
-void solve(int x, int y, int z)
+void func(int x, int y, int n)
 {
-    if (check(x, y, z)) {
-        cnt[paper[x][y] + 1] += 1;
+    if(check(x, y, n)){
+        int val = v[x][y];
+        cnt[val+1]++;
         return;
     }
-    int n = z / 3;
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            solve(x + i * n, y + j * n, n);
+    int z = n/3;
+    for(int i = 0; i<3; i++){
+        for(int j =0; j<3; j++){
+            func(x+z*i, y+z*j, z);
+        }
+    }
 }
 
-int main(void) {
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> N;
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            cin >> paper[i][j];
-    solve(0, 0, N);
-    for (int i = 0; i < 3; i++) cout << cnt[i] << "\n";
+
+    int num;
+    cin>>num;
+    for(int i = 0; i < num; i++){
+        for(int j = 0; j < num; j++){
+            cin>>v[i][j];
+        }
+    }
+
+    func(0, 0, num);
+    cout<<cnt[0]<<'\n';
+    cout<<cnt[1]<<'\n';
+    cout<<cnt[2]<<'\n';
+
+    return 0;
 }
