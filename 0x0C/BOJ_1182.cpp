04cbb0bbc907f087950bdde0876c8cad4318addd
yulimmm <89307538+yulimmm@users.noreply.github.com>
2024-08-21 22:54:22 +0900

    Update BOJ_1182.cpp

--- 0x0C/BOJ_1182.cpp
+++ 0x0C/BOJ_1182.cpp
@@ -6,21 +6,21 @@ int arr[30];
 int cnt = 0;
 
 void func(int cur, int tot){
-    if(cur == n){
-        if(tot == s) cnt++;
-        return;
-    }
-    func(cur+1, tot);
-    func(cur+1, tot+arr[cur]);
+  if(cur == n){
+    if(tot == s) cnt++;
+    return;
+  }
+  func(cur+1, tot);
+  func(cur+1, tot+arr[cur]);
 }
 
-int main()
-{
-    cin>>n>>s;
-    for(int i = 0; i<n; i++)
-        cin>>arr[i];
-    func(0,0);
-    if(s==0) cnt--;
-    cout<<cnt;
-    return 0;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> s;
+  for(int i = 0; i < n; i++)
+    cin >> arr[i];
+  func(0, 0);
+  if(s == 0) cnt--;
+  cout << cnt;
 }
