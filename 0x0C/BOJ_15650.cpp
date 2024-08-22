95c5ee3db6182c5bbd16a89a4f6fe425f34e0082
yulimmm <89307538+yulimmm@users.noreply.github.com>
2024-08-22 22:22:49 +0900

    Update BOJ_15650.cpp

--- 0x0C/BOJ_15650.cpp
+++ 0x0C/BOJ_15650.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int n, m;
+int m,n;
+int isused[10];
 int arr[10];
-bool isused[10];
 
-void func(int k){ // 현재 k개까지 수를 택했음.
-  if(k == m){ // m개를 모두 택했으면
-    for(int i = 0; i < m; i++)
-      cout << arr[i] << ' '; // arr에 기록해둔 수를 출력
-    cout << '\n';
+void func(int k)
+{
+  if(k==n){
+    for(int i = 0; i < n; i++){
+      cout<<arr[i]<<' ';
+    }
+    cout<<'\n';
     return;
   }
-  int st = 1; // 시작지점, k = 0일 때에는 st = 1
-  if(k != 0) st = arr[k-1] + 1; // k != 0일 경우 st = arr[k-1]+1
-  for(int i = st; i <= n; i++){ 
-    if(!isused[i]){ // 아직 i가 사용되지 않았으면
-      arr[k] = i; // k번째 수를 i로 정함
-      isused[i] = 1; // i를 사용되었다고 표시
-      func(k+1); // 다음 수를 정하러 한 단계 더 들어감
-      isused[i] = 0; // k번째 수를 i로 정한 모든 경우에 대해 다 확인했으니 i를 이제 사용되지않았다고 명시함.
+  for(int i = k+1; i <= m; i++){
+    if(!isused[i]&&arr[k-1]<i){
+      arr[k] = i;
+      isused[i] = 1;
+      func(k+1);
+      isused[i] = 0;
     }
   }
 }
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> m;
+int main()
+{
+  ios::sync_with_stdio(0); cin.tie(0);
+  cin >> m >> n;
   func(0);
+  return 0;
 }
