4735ca3cd41d8d09f4b3cf1eba95aa7502eb6645
yulimmm <89307538+yulimmm@users.noreply.github.com>
2024-10-03 14:25:54 +0900

    Update KAKAO_Gift.cpp

--- Programmers/LEVEL1/KAKAO_Gift.cpp
+++ Programmers/LEVEL1/KAKAO_Gift.cpp
@@ -1,53 +1,72 @@
-#include <string>
+//2024 KAKAO WINTER INTERNSHIP 가장 많이 받은 선물
 #include <vector>
+#include <map>
+#include <string>
 #include <sstream>
 
 using namespace std;
 
-struct gift{
-    string name;
-    int give = 0;
-    int take = 0;
-    int rate; //선물지수 = 준 거 - 받은 거 
-    bool expect[50];
-};
-
 int solution(vector<string> friends, vector<string> gifts) {
-    int totalFriends = friends.size();
-    gift friendList[totalFriends];
-    
-    for(int i = 0; i < totalFriends; i++){
-        friendList[i].name = friends[i];
+    map<string, int>nameTable;
+    int totalF = friends.size();
+    for(int i = 0; i < totalF; i++){
+        nameTable.insert({friends[i], i});
     }
-    
+    int giftTable[50][50] = {0};
+
     for(int i = 0; i < gifts.size(); i++){
-        for(int k = 0; k < totalFriends; k++){
-            stringstream ss(gifts[i]);
-            string give;
-            string take;
-            ss >> give >> take;
-            if(friendList[i].name == give){
-                friendList[i].give++;
+        stringstream ss(gifts[i]);
+        string give;
+        string take;
+        ss >> give >> take;
+        auto giveID = nameTable.find(give);
+        auto takeID = nameTable.find(take);
+        //cout << give <<": "<<giveID->second <<endl;
+        giftTable[giveID->second][takeID->second]++;
+    }
+    
+    //선물지수 계산
+    int giftRate[50] = {0};
+    int give[50] = {0};
+    int take[50] = {0};
+    for(int i = 0; i < totalF; i++){
+        for(int j = 0; j < totalF; j++){
+            give[i] += giftTable[i][j];
+            take[i] += giftTable[j][i];
+        }
+    }
+
+    for(int i = 0; i < totalF; i++){
+        giftRate[i] = give[i]-take[i];
+        //cout<<i<<"번째 선물지수: "<<giftRate[i]<<'\n';
+    }
+
+    int expect[50] = {0};
+    for(int i = 0; i < totalF; i++){
+        for(int j = 0; j < totalF; j++){
+            if(giftTable[i][j] > giftTable[j][i]){
+                expect[i]++;
             }
-            if(friendList[i].name == take){
-                friendList[i].take++;
+            else if(giftTable[i][j] < giftTable[j][i]){
+                expect[j]++;
+            }
+            else{ // 같다면 선물지수 비교
+                if(giftRate[i]>giftRate[j]){
+                    expect[i]++;
+                }
+                else if(giftRate[i]<giftRate[j]){
+                    expect[j]++;
+                }
             }
         }
     }
-    
-    int maxRate = -10000;
-    int maxRateIndex;
-    for(int i = 0; i < totalFriends; i++){
-        friendList[i].rate = friendList[i].give - friendList[i].take;
-        if(maxRate<friendList[i].rate) {
-            maxRate = friendList[i].rate;
-            maxRateIndex = i;
+    int solution = 0;
+    //cout<<"선물 받을 예정:";
+    for(int i = 0; i < totalF; i++){
+        //cout<<expect[i]<<" ";
+        if(expect[i]>solution){
+            solution = expect[i];
         }
     }
-    
-    int answer = 0;
-    for(int i = 0; i < totalFriends; i++){
-        if(friendList[maxRateIndex].rate>friendList[i].rate) answer++;
-    }
-    return answer;
+    return solution/2;
 }
