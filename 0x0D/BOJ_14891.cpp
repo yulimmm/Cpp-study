0f6f819c1c6fac914fb8e90e781379ffc82242da
yulimmm <89307538+yulimmm@users.noreply.github.com>
2024-10-16 23:39:07 +0900

    Update BOJ_14891.cpp

--- 0x0D/BOJ_14891.cpp
+++ 0x0D/BOJ_14891.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int wheel[4][8];
-int cur[4] = {0}; //12시 방향
+vector<vector<int>>wheel;
+vector<int>cursor = {0};
 
-void redefine()
+void move(int num, int dir)
 {
-
+  if(num == 0){
+    
+    cur[num]+=dir;
+  }
 }
 
 int main()
@@ -14,7 +18,9 @@ int main()
   ios::sync_with_stdio(0); cin.tie(0);
   for(int i = 0; i < 4; i++){
     for(int j = 0; j < 8; j++){
-      cin >> wheel[i][j];
+      int tmp;
+      cin >> tmp;
+      wheel[i][j].push(tmp);
     }
   }
   int cnt, num, dir;
@@ -22,11 +28,7 @@ int main()
   for(int i = 0; i < cnt; i++){
     cin >> num >> dir;
     int num = num-1;
-    if(dir == -1){
-      cur[num] = cur[num] -1;
-      
-    }
-
+    move(num, dir);
     
   }
 
